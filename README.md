56b7758541cb3e5b227e26e3873a4e20ea34735d
Takao Akaki <mongonta@gmail.com>
2021-12-23 13:38:25 +0900

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # stack-chan-tester
+スタックチャンを作成するときに、サーボのテストを行うためのアプリケーションです。<br>
 stack-chan test application for pwm servo
+
+※ ArduinoFrameworkのみです。
+
+# 使い方
+ボタンA： X軸、Y軸のサーボを90°に回転します。
+ボタンB： X軸は0〜180, Y軸は90〜50まで動きます。
+ボタンC： ランダムで動きます。
+
+# 必要なライブラリ
+- [M5Stack-Avatar](https://github.com/meganetaaan/m5stack-avatar)
+- [ServoEasing](https://github.com/ArminJo/ServoEasing)
+- [ESP32Servo](https://github.com/madhephaestus/ESP32Servo)
+
+# ビルド方法
+ 下記のリンクを参照してArduinoIDEでビルドできます。
+ [初心者向けM5Stack Core2の始め方（ArduinoIDE編）](https://raspberrypi.mongonta.com/howto-start-m5stack-core2arduinoide/)
+
+# 書き込み後再起動を繰り返す場合
+ 2021/12現在、Arduino-esp32のバージョンをv2.0.0以上にすると、書き込み後再起動を繰り返すという現象が見られます。
+
+## 対処①
+ ボードマネージャーでarduino-esp32 ver.1.0.6に戻してビルドする。
+
+## 対処②
+ librariesの中にあるm5stack-avatar/src/Avatar.cppを修正してください。修正箇所は下記のリンクにあります。
+ - [fix platformio link error #66](https://github.com/meganetaaan/m5stack-avatar/pull/66/commits/f28efa87d482a730237565a666d67d7422e638f4)
+
+# author
+ Takao Akaki
+
+# LICENSE
+ MIT
---
+++ Stackchan_tester/Stackchan_tester.ino
@@ -0,0 +1,105 @@
+#include <Arduino.h>
+
+#if defined(ARDUINO_M5STACK_Core2)
+  #include <M5Core2.h>
+  #define SERVO_PIN_X 13
+  #define SERVO_PIN_Y 14
+#elif defined( ARDUINO_M5STACK_FIRE )
+  #include <M5Stack.h>
+  #define SERVO_PIN_X 22
+  #define SERVO_PIN_Y 21
+#elif defined( ARDUINO_M5Stack_Core_ESP32 )
+  #include <M5Stack.h>
+  #define SERVO_PIN_X 22
+  #define SERVO_PIN_Y 21
+#endif
+
+#include <Avatar.h> // https://github.com/meganetaaan/m5stack-avatar
+#include <ServoEasing.hpp> // https://github.com/ArminJo/ServoEasing       
+
+using namespace m5avatar;
+Avatar avatar;
+
+#define START_DEGREE_VALUE_X 90
+#define START_DEGREE_VALUE_Y 90
+
+ServoEasing servo_x;
+ServoEasing servo_y;
+
+bool random_move = false;
+
+const char* lyrics[] = { "BtnA:MoveTo90  ", "BtnB:ServoTest  ", "BtnC:RandomMode  "};
+const int lyrics_size = sizeof(lyrics) / sizeof(char*);
+int lyrics_idx = 0;
+
+void setup() {
+
+#if defined(ARDUINO_M5STACK_Core2)
+  M5.begin(true, true, true, false, kMBusModeOutput);
+  // M5.begin(true, true, true, false, kMBusModeInput);
+#elif defined( ARDUINO_M5STACK_FIRE ) || defined( ARDUINO_M5Stack_Core_ESP32 )
+  M5.begin(true, true, true, false); // Grove.Aを使う場合は第四引数(I2C)はfalse
+#endif
+  if (servo_x.attach(SERVO_PIN_X, START_DEGREE_VALUE_X, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE)) {
+    Serial.print("Error attaching servo x");
+  }
+  if (servo_y.attach(SERVO_PIN_Y, START_DEGREE_VALUE_Y, DEFAULT_MICROSECONDS_FOR_0_DEGREE, DEFAULT_MICROSECONDS_FOR_180_DEGREE)) {
+    Serial.print("Error attaching servo y");
+  }
+  servo_x.setEasingType(EASE_QUADRATIC_IN_OUT);
+  servo_y.setEasingType(EASE_QUADRATIC_IN_OUT);
+  setSpeedForAllServos(60);
+  avatar.init();
+}
+
+void loop() {
+  M5.update();
+  if (M5.BtnA.wasPressed()) {
+    random_move = false;
+    servo_x.setEaseTo(90);
+    servo_y.setEaseTo(90);
+    synchronizeAllServosStartAndWaitForAllServosToStop();
+  }
+  if (M5.BtnB.wasPressed()) {
+    random_move = false;
+    for (int i=0; i<2; i++) {
+      avatar.setSpeechText("X 90 -> 0  ");
+      servo_x.easeTo(0);
+      avatar.setSpeechText("X 0 -> 180  ");
+      servo_x.easeTo(180);
+      avatar.setSpeechText("X 180 -> 90  ");
+      servo_x.easeTo(90);
+      avatar.setSpeechText("Y 90 -> 50  ");
+      servo_y.easeTo(50);
+      avatar.setSpeechText("Y 50 -> 90  ");
+      servo_y.easeTo(90);
+    }
+  }
+  if (M5.BtnC.wasPressed()) {
+    random_move = !random_move;
+  }
+
+  if (random_move) {
+    int x = random(180);
+    int y = random(40);
+    M5.update();
+    if (M5.BtnC.wasPressed()) {
+      random_move = false;
+    }
+    servo_x.setEaseTo(x);
+    servo_y.setEaseTo(y + 40);
+    synchronizeAllServosStartAndWaitForAllServosToStop();
+    int delay_time = random(10);
+    delay(2000 + 100*delay_time);
+    avatar.setSpeechText("Stop BtnC");
+  }
+  if (!random_move) {
+    const char* l = lyrics[lyrics_idx++ % lyrics_size];
+    avatar.setSpeechText(l);
+    avatar.setMouthOpenRatio(0.7);
+    delay(200);
+    avatar.setMouthOpenRatio(0.0);
+    delay(2000);
+  }
+
+}
